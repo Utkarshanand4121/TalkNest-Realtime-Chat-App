092c9ac9d38984d822bcb5342fb29b0701731f8f
Utkarsh Anand <utkarshanand4121@gmail.com>
2025-01-15 00:05:38 +0530

    Implement user signup and login controllers, add user model, and create utility classes for error handling and responses

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,11 +1,45 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.module.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const signupController = asyncHandler(async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if ([fullName, email, password].some((field) => field?.trim === "")) {
+    throw new ApiError(400, "Some fields are missing.");
+  }
+
+  if (password.length() < 5) {
+    throw new ApiError(400, "Password is must be atleast 6 character.");
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    throw new ApiError(400, "User already exists");
+  }
+
+  // hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({
+    fullName,
+    email,
+    password: hashPassword,
+  });
+  if (newUser) {
+    // generate jwt token here
+  } else {
+    throw new ApiError(400, "Invalid user data");
+  }
+});
+
+const loginController = (req, res) => {
+  res.send("login route");
 };
 
-export const login = (req, res) => {
-    res.send("login route");
-}
+const logoutController = (req, res) => {
+  res.send("logout route");
+};
 
-export const logout = (req, res) => {
-    res.send("logout route");
-}
+export { signupController, loginController, logoutController };
